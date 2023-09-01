cacad714b66222daf91dbffcec4ab57c34f3481e
longlee10 <longquan98@gmail.com>
2023-09-01 12:28:32 -0400

    Defining layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
-
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+`"nav nav" "aside main"`;
 function App() {
-  return <Button colorScheme="teal">Submit</Button>;
+  return (
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+    >
+      <GridItem area={"nav"} bg={"coral"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"teal"}>
+          Side
+        </GridItem>
+      </Show>
+
+      <GridItem area={"main"} bg={"deeppink"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
