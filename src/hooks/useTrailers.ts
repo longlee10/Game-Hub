11b23af7fa7a75041e5380cd8e875e0c13ad0215
longquan98@gmail.com <longquan98@gmail.com>
2023-09-24 16:58:28 -0400

    Building game trailer

---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../services/api-client";
+import { Trailer } from "../entities/Trailer";
+
+const useTrailers = (gameID: number) => {
+  const apiClient = new APIClient<Trailer>(`/games/${gameID}/movies`);
+  return useQuery({
+    queryKey: ["trailers", gameID],
+    queryFn: apiClient.fetchAll,
+  });
+};
+
+export default useTrailers;
